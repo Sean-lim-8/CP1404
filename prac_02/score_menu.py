cf6bd0f796963b3148485f835918b802291a2277
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-09-28 01:22:23 +0800

    imported function determine_score from score.py folder, and created a menu to display choices.

---
+++ prac_02/score_menu.py
@@ -0,0 +1,7 @@
+"""
+CP1404 - Practical 2
+Program to display a menu to get a score and print results and stars depending on the score.
+"""
+from score import determine_score
+
+MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
