e68a2ac1bc14077b38ac84dd842f640d81e4cd2f
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-09-28 01:10:55 +0800

    Refactored program to determine score status to use functions.

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,22 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    message = determine_score(score)
+    print(message)
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        message = "Invalid score"
+    elif score >= 90:
+        message = "Excellent"
+    elif score >= 50:
+        message = "Passable"
+    else:
+        message = "Bad"
+    return message
+
+
+main()
