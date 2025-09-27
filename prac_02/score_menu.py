fdcc19e8bc4d52a3e11a796702eb04458b2fa0bf
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-09-28 02:13:41 +0800

    Remove import and rewrote the function to get and validate user score instead.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,14 +2,48 @@
 CP1404 - Practical 2
 Program to display a menu to get a score and print results and stars depending on the score.
 """
-from score import determine_score
 
 MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 
+def get_valid_score():
+    """validates user's score"""
+    score = float(input("Enter your score: "))
+    while score < 0 or score > 100:
+        print("Please enter a valid score between 0 and 100")
+        score = float(input("Enter your score: "))
+    return score
+
+
 def main():
+    score = 0
     print(MENU)
-    choice = input("Please choose an option: ").upper()
-    score = determine_score(MENU)
+    choice = input("Choose an option: ").upper()
+
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            print_results(score)
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid choice")
+
+        print(MENU)
+        choice = input("Choose an option: ").upper()
+
+    print("Farewell")
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        message = "Invalid score"
+    elif score >= 90:
+        message = "Excellent"
+    elif score >= 50:
+        message = "Passable"
+    else:
+        message = "Bad"
+    return message
 
 def print_results(score):
     results = determine_score(score)
@@ -18,3 +52,4 @@ def print_results(score):
 def print_stars(score):
     print(int(score) * "*")
 
+main()
