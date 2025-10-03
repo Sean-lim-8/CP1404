94ef0e771433baedca8fa18b73386e0d9c0df82f
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-04 02:57:43 +0800

    Completed question 2 and 3.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,8 +1,12 @@
 import random
 
-#print(random.randint(5,20)) # 8
-#print(random.randrange(3,10,2)) # 9
+print(random.randint(5,20)) # 8
+print(random.randrange(3,10,2)) # 9
 print(random.uniform(2.5,5.5)) # 2.524204796960463
 
-# The number i saw was 8. The smallest number i could have seen is 5 and the largest would have been 20.
-#
+#Q1. The number i saw was 8. The smallest number i could have seen is 5 and the largest would have been 20.
+
+#Q2. The number i saw was 9. The smallest number i could have seen is 3 and the largest would have been 20. No, line 2 could not produce a 4.
+
+#Q3. I saw 2.524204796960463. The smallest number i could have seen is 2.5 and the largest would have been 5.5.
+
