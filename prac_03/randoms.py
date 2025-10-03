95a1686031ac07d23f26f7c4542ee2252547f690
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-04 02:23:09 +0800

    Ran the program for each line one at a time and answered question 1.

---
+++ prac_03/randoms.py
@@ -0,0 +1,8 @@
+import random
+
+#print(random.randint(5,20)) # 8
+#print(random.randrange(3,10,2)) # 9
+print(random.uniform(2.5,5.5)) # 2.524204796960463
+
+# The number i saw was 8. The smallest number i could have seen is 5 and the largest would have been 20.
+#
