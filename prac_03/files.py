b8ce158e14590e884cce572661bb0c21be21eaba
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-05 00:35:38 +0800

    Completed question 4.

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,25 +3,32 @@ CP1404 - Practical
 Program to create and edit files
 """
 
-# Question 1
-name = input("Enter name: ")
-out_file = open(f"name.txt", "w")
-out_file.write(name)
-out_file.close()
+# # Question 1
+# name = input("Enter name: ")
+# out_file = open(f"name.txt", "w")
+# out_file.write(name)
+# out_file.close()
+#
+# # Question 2
+# in_file = open("name.txt", "r")
+# name = in_file.read()
+# in_file.close()
+# print(f"Hi {name}!")
+#
+# # Question 3
+# with open("numbers.txt", "r") as in_file:
+#     number1 = int(in_file.readline())
+#     number2 = int(in_file.readline())
+#     result = number1 + number2
+#
+# print(f"The sum of the numbers is: {result}.")
 
-# Question 2
-in_file = open("name.txt", "r")
-name = in_file.read()
-in_file.close()
-print(f"Hi {name}!")
+# Question 4
+total = 0
 
-# Question 3
 with open("numbers.txt", "r") as in_file:
-    number1 = int(in_file.readline())
-    number2 = int(in_file.readline())
-    result = number1 + number2
+    for line in in_file:
+        total += int(line.strip())
 
-print(f"The sum of the numbers is: {result}.")
-
-
+print(f"The sum of all the numbers is: {total}.")
 
