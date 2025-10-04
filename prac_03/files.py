2b808404852ed59dc38957d67fb86ed2333a586d
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-05 00:36:29 +0800

    Edited some comments.

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,25 +3,25 @@ CP1404 - Practical
 Program to create and edit files
 """
 
-# # Question 1
-# name = input("Enter name: ")
-# out_file = open(f"name.txt", "w")
-# out_file.write(name)
-# out_file.close()
-#
-# # Question 2
-# in_file = open("name.txt", "r")
-# name = in_file.read()
-# in_file.close()
-# print(f"Hi {name}!")
-#
-# # Question 3
-# with open("numbers.txt", "r") as in_file:
-#     number1 = int(in_file.readline())
-#     number2 = int(in_file.readline())
-#     result = number1 + number2
-#
-# print(f"The sum of the numbers is: {result}.")
+# Question 1
+name = input("Enter name: ")
+out_file = open(f"name.txt", "w")
+out_file.write(name)
+out_file.close()
+
+# Question 2
+in_file = open("name.txt", "r")
+name = in_file.read()
+in_file.close()
+print(f"Hi {name}!")
+
+# Question 3
+with open("numbers.txt", "r") as in_file:
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+    result = number1 + number2
+
+print(f"The sum of the numbers is: {result}.")
 
 # Question 4
 total = 0
