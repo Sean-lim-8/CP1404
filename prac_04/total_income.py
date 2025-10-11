599c6f57ffa9ff6883d7a315688316b7b58029ee
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-11 23:33:38 +0800

    Refactored the code to print the income report into a function.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,6 +13,11 @@ def main():
         income = float(input(f"Enter income for month {month}: ")) # changed to an f-string
         incomes.append(income)
 
+    print_income_report(incomes, number_of_months)
+
+
+def print_income_report(incomes, number_of_months):
+    """This function prints the income report"""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
