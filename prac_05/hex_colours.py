449bf88640f54e73dbd7f0e429ed51e2a0930b87
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-19 01:04:29 +0800

    Created hex_colours.py file and added a dictionary for colours to code.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical
+Colour codes in a dictionary
+"""
+
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba",
+                  "Acid Green": "#b0bf1a",
+                  "Alice Blue": "#f0f8ff",
+                  "Baby Blue": 	"#89cff0",
+                  "Baby Pink": "#f4c2c2",
+                  "Banana Mania": "#fae7b5",
+                  "Cadet Grey": "#91a3b0",
+                  "CadetBlue": "#5f9ea0",
+                  "Cadmium Green": "#006b3c",
+                  "Camel": "#c19a6b"}
+
