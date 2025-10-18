91f475cd7ceb93efbab97ac153bc8ed9466a8266
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-19 02:17:25 +0800

    Completed initial program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,3 +5,18 @@ Estimate: 20 minutes
 Actual:
 """
 
+text = input("Enter text: ")
+words = text.lower().split()
+word_count = {}
+
+for word in words:
+    word = word.strip(",.!?\;""'")
+    word_count[word] = word_count.get(word, 0) + 1
+
+print(f"Text: {text}")
+for word, count in word_count.items():
+    print(f"{word}: {word_count[word]}")
+
+
+
+
