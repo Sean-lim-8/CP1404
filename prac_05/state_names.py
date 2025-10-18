fe52f2d1cfdfe39903d520365e64c7fa2ac7b97f
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-19 00:47:42 +0800

    Refactored the dictionary to follow PEP 8 format. Changed the print code to print in neat lines.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,9 +5,16 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+CODE_TO_NAME = {"QLD": "Queensland",
+                "NSW": "New South Wales",
+                "NT": "Northern Territory",
+                "WA": "Western Australia",
+                "ACT": "Australian Capital Territory",
+                "VIC": "Victoria", "TAS": "Tasmania",
+                "SA": "South Australia"}
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
