972e64d81f784a9e200aac0dda33418795b680dd
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-10-19 02:23:01 +0800

    Added a code to sort the words and another code to store the maximum length of a word. Completed practice 5 word occurrences exercise.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,7 +2,7 @@
 CP1404 Practical 5
 Word Occurrences
 Estimate: 20 minutes
-Actual:
+Actual: 28 minutes
 """
 
 text = input("Enter text: ")
@@ -14,8 +14,14 @@ for word in words:
     word_count[word] = word_count.get(word, 0) + 1
 
 print(f"Text: {text}")
+
+sorted_words = list(word_count.keys())
+sorted_words.sort()
+
+word_length = max(len(word) for word in sorted_words)
+
 for word, count in word_count.items():
-    print(f"{word}: {word_count[word]}")
+    print(f"{word:{word_length}} : {word_count[word]}")
 
 
 
