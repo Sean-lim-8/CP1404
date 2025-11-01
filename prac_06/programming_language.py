4354dd4f05d27bf5ee96f684d379087bfdacdebe
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-02 02:27:04 +0800

    Added programming_language class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,13 @@ Programming language
 Estimate: 20 minutes
 Actual:
 """
+
+class ProgrammingLanguage:
+    """Represent programming languages"""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise programming language with information"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
