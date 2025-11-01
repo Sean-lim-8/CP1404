081a4e1a3720a1fbfabcb62be2761d39dda71ffc
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-02 04:10:06 +0800

    Added a loop to get user input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,3 +5,20 @@ Estimate: 25 minutes
 Actual:
 """
 
+from guitar import Guitar
+
+def main():
+    """Program to manage guitar collection"""
+    guitars = []
+
+    print("My guitars!")
+
+    name =  input("Name: ")
+    while name != "":
+        year = input("Year: ")
+        cost = input("Cost: ")
+
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
