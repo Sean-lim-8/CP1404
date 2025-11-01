b5c7175dbc70433f67790591236c904f22ba7e30
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-02 02:56:44 +0800

    Added a class for guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,3 +4,11 @@ Guitar
 Estimate: 30 minutes
 Actual:
 """
+
+class Guitar:
+    """Represent guitar"""
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
