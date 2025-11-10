682cfa1089656510bcae53c883a2fd8725b0d548
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-10 19:38:48 +0800

    Added a Guitar class to represent name, year and age.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,3 +4,12 @@ MyGuitar
 Estimate: 30 minutes
 Actual:
 """
+
+class Guitar:
+    """Represent guitar with name, year, cost"""
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
