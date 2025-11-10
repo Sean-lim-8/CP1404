ef6e8c76dcf269941cad2dee328c92c84fe660c2
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-10 21:43:57 +0800

    Edited load_projects function. Added an option to the menu and added save_projects function.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -36,6 +36,11 @@ def main():
             projects = load_projects(filename)
             print(f"Loaded {len(projects)} projects from {filename}.")
 
+        elif choice == "S":
+            filename = input("Filename: ")
+            save_projects(filename, projects)
+            print(f"Projects saved to {filename}")
+
 
 def load_projects(filename):
     """Load projects from a file"""
@@ -46,11 +51,23 @@ def load_projects(filename):
             for line in file:
                 parts = line.strip().split()('\t')
                 if len(parts) == 5:
+                    name = parts[0]
                     start_date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
                     priority = int(parts[2])
-                    cost_estimate = float(parts[3])
-                    completion_percentage = int(parts[4])
-                    projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+                    cost = float(parts[3])
+                    completion = int(parts[4])
+                    projects.append(Project(name, start_date, priority, cost, completion))
     except FileNotFoundError:
         print(f"File {filename} not found.")
     return projects
+
+def save_projects(filename, projects):
+    """Save projects to a file"""
+    with open(filename, 'w') as in_file:
+        in_file.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
+        for project in projects:
+            in_file.write(f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t"
+                       f"{project.priority}\t{project.cost}\t{project.completion}\n")
+    print(f"Saved {len(projects)} projects to {filename}.")
+
+main()
