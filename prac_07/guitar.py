cfaa6e24aafdb99026af5ae543929d8ca062610c
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-10 19:29:43 +0800

    Copied guitar.py from prac 6 to prac 7 folder.

---
+++ prac_07/guitar.py
@@ -0,0 +1,27 @@
+"""
+CP1404 - Practical 6
+Guitar
+Estimate: 30 minutes
+Actual:
+"""
+
+class Guitar:
+    """Represent guitar"""
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Returns a string representation of guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Returns the age of the guitar in years"""
+        return 2022 - self.year
+
+    def is_vintage(self):
+        """Determines if the guitar is vintage or not"""
+        return self.get_age() >= 50
+
+
