8bd245395f5cb7d388350eb7888cd6981bfc956b
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-10 21:30:46 +0800

    Created Project class.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,12 @@
-from datetime import date
+from datetime import datetime
 
 class Project:
-    """Represents project details"""
+    """Represents a project"""
     def __init__(self, name, start_date, priority, cost, completion):
+        """Initialise a project instance"""
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost = cost
         self.completion = completion
+
