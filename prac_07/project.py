e7392192eb86c3a44a77b50737a616ef7f439e4b
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-10 20:52:42 +0800

    Created a class that represents project details.

---
+++ prac_07/project.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+class Project:
+    """Represents project details"""
+    def __init__(self, name, start_date, priority, cost, completion):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
