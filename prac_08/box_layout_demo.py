957058b79e4dd12c4ade6d308eba019cebf03bc2
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-17 01:47:43 +0800

    completed tasks 4 to 9.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,10 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        """Handle the greet action"""
+        print("Greet")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
 
 BoxLayoutDemo().run()
