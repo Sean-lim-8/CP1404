621d56d9ce31d29e0f8ba0c8a3352ef3774b698b
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-17 20:58:18 +0800

    Dynamic labels completed.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,7 +2,7 @@
 Cp1404 Practical 8
 Dynamic labels
 Estimate: 30 minutes
-Actual:
+Actual: 20 minutes
 """
 
 from kivy.app import App
@@ -14,6 +14,16 @@ class DynamicLabelsApp(App):
         super().__init__(**kwargs)
         self.seafood = ["fish", "crab", "lobster", "eel", "squid"]
 
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
 
+    def create_labels(self):
+        """Creates labels for names in the list"""
+        for name in self.seafood:
+            label = Label(text=name)
+            self.root.ids.main.add_widget(label)
 
 DynamicLabelsApp().run()
