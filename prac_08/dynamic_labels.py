283f0d57664e8c045bfc6f1d9f3cd33949985f50
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-17 20:46:52 +0800

    Created a class for DynamicLabelsAPp.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -4,3 +4,16 @@ Dynamic labels
 Estimate: 30 minutes
 Actual:
 """
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.lang import Builder
+
+class DynamicLabelsApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.seafood = ["fish", "crab", "lobster", "eel", "squid"]
+
+
+
+DynamicLabelsApp().run()
