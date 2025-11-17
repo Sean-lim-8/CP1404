ba085b0d8f3dc82ee5bd6f48428dcdb1afdb7934
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-17 20:01:57 +0800

    Wrote code to read the convert_miles_km.kv file.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,6 +1,20 @@
 """
 CP1404 Practical 8
-Convert miles to kilometers
+Convert miles to kilometres
 Estimate: 30 minutes
 Actual:
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+MILES_TO_KM = 1.60934
+
+class MilesConverterApp(App):
+    def build(self):
+        self.title = "Miles to Kilometres"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+
+MilesConverterApp().run()
