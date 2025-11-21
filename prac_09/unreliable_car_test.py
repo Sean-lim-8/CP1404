07dad4dc51add3b5c179fd05cb6428e42b9f66ce
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-22 03:28:25 +0800

    Created a function for different cars to test.

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -4,3 +4,11 @@ Unreliable Car Test
 """
 
 from prac_09.unreliable_car import UnreliableCar
+
+def main():
+    """Tests that Unreliable Car initialises correctly"""
+    good_car = UnreliableCar("Good Car", 100,  90)
+    weird_car = UnreliableCar("Weird Car", 100, 50)
+    bad_car = UnreliableCar("Bad Car", 100, 30)
+
+
