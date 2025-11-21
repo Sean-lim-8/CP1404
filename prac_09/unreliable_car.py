c013df638677f218520f352a467d6cf1435697a8
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-22 03:01:36 +0800

    Added __init__ to UnreliableCar class.

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,3 +4,13 @@ Unreliable Car
 """
 
 from prac_09.car import Car
+from random import randint
+
+class UnreliableCar(Car):
+    """Represent an unreliable car"""
+
+    def __init__(self, name, fuel, reliability):
+            """Initialize an UnreliableCar instance"""
+            super().__init__(name, fuel)
+            self.reliability = reliability
+
