740d05d2b4715147ae62f2d026db5ac4370dec7e
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-22 01:56:06 +0800

    Updated taxi_test.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -6,7 +6,7 @@ taxi test
 from prac_09.taxi import Taxi
 
 def main():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     print("current fare:", my_taxi.get_fare())
