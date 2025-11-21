31f0debc02c780b237964ca02e3d91062ad71671
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-22 01:14:15 +0800

    taxi_test.py completed

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -9,8 +9,12 @@ def main():
     my_taxi = Taxi("Prius 1", 100, 1.23)
     my_taxi.drive(40)
     print(my_taxi)
+    print("current fare:", my_taxi.get_fare())
 
-
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+    print("current fare:", my_taxi.get_fare())
 
 main()
 
