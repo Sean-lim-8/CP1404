b714ff2a9c9db8280f7adc08218e138075070aeb
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-23 19:22:36 +0800

    Added init to silver service taxi class

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,3 +2,13 @@
 CP1404 Practical 9
 Silver service taxi test
 """
+
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a silver service Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
