c8ca66c6ab51ad6b2d3a8fe0321af4193063c089
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-23 19:45:22 +0800

    silver_service_taxi.py completed.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,6 +6,7 @@ Silver service taxi test
 from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
+    """represent silver service taxi with a higher fare"""
 
     flagfall = 4.5
 
@@ -15,8 +16,14 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
 
+    def get_fare(self):
+        """calculates the fare including flagfall"""
+        return super().get_fare() + self.flagfall
+
     def __str__(self):
         """returns a string representation of the Taxi instance."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+
 
 
