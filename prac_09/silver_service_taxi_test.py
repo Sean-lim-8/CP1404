2b1b2291a24ac42b25376f5ba1b1e603039e15b9
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-23 19:23:43 +0800

    Added a flagfall variable.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -7,8 +7,12 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
 
+    flagfall = 4.5
+
     def __init__(self, name, fuel, fanciness):
         """Initialise a silver service Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+
