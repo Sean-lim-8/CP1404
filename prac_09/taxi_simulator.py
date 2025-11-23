026f1e80de0f0688165752b30aec4c01dbed2adf
Sean-lim-8 <tjinhsiensean.lim@my.jcu.edu.au>
2025-11-23 21:00:06 +0800

    Added main function for taxi simulator program.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,3 +6,14 @@ Taxi Simulator
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
+def main():
+    """run the taxi simulator program"""
+    current_taxi = None
+    bill = 0
+
+    taxis = [Taxi("prius", 100),
+             SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+
+    print("Let's drive!")
+
